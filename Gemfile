9fa254b0caed8a678eef78700ff237de67a1abbd
ben wanicur <bwanicur@gmail.com>
2015-04-23 21:17:19 -0700

    trying to add font-awesome icons

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'acts_as_list'
-
+gem 'font-awesome-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -44,4 +44,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,10 +1,21 @@
 module GamesHelper
   def cell_status(x, y)
     if @game.completed?
-      [("hit" if @game.hit_at?(x, y)), ("ship" if @game.ship_at?(x, y))].compact.join(" ")
-    else
-      "hit" if @game.hit_at?(x, y)
       # [("hit" if @game.hit_at?(x, y)), ("ship" if @game.ship_at?(x, y))].compact.join(" ")
+      if @game.hit_at?(x,y)
+        "hit"
+      elsif @game.hit_at?(x,y) && @game.ship_at?(x,y)
+        "hit_ship"
+      elsif @game.ship_at?(x,y)
+        "ship"
+      end
+    else
+      # "hit" if @game.hit_at?(x, y)
+      if @game.hit_at?(x, y)
+        "hit"
+      elsif @game.ship_at?(x,y)
+        "ship"
+      end
     end
   end
 end
