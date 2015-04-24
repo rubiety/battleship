1dd090c5a95e4e5a29f8fc1b35e668124ec65979
Ben Hughes <ben@railsgarden.com>
2015-04-23 21:02:57 -0700

    Final refinement

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,8 +3,8 @@ module GamesHelper
     if @game.completed?
       [("hit" if @game.hit_at?(x, y)), ("ship" if @game.ship_at?(x, y))].compact.join(" ")
     else
-      # TODO: "hit" if @game.hit_at?(x, y)
-      [("hit" if @game.hit_at?(x, y)), ("ship" if @game.ship_at?(x, y))].compact.join(" ")
+      "hit" if @game.hit_at?(x, y)
+      # [("hit" if @game.hit_at?(x, y)), ("ship" if @game.ship_at?(x, y))].compact.join(" ")
     end
   end
 end
